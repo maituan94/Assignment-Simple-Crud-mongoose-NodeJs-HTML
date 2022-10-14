01ca8a3ae30fa316dc7bbc5ed53675a7d864f4a0
Tuan Mai <>
2022-10-14 11:38:48 -0400

    update statusCode to getProvinces

--- src/server/controllers/general.controler.js
+++ src/server/controllers/general.controler.js
@@ -1,8 +1,9 @@
 import { provinces } from "../enum/index.js"
+import { statusCode } from "../enum/index.js"
 
 const getProvinces = (req, res) => {
-    return res.status(200).json({
-        statusCode: 200,
+    return res.status(statusCode.success).json({
+        statusCode: statusCode.success,
         data: provinces
     })
 }
